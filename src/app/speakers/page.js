9292e3697f57f265a77e9219df215356539ba490
iddev5 <ayushbardhan5@gmail.com>
2025-03-31 16:35:17 +0530

    Add 2 speakers

--- src/app/speakers/page.js
+++ src/app/speakers/page.js
@@ -2,15 +2,30 @@ import Image from "next/image"
 
 function SpeakerCard({photo, name, desig}) {
     return <div className="flex flex-col items-center gap-2">
-        <div className="w-[150px] h-[150px] rounded-full bg-red-500"></div>
+        <div className="w-[150px] h-[150px] rounded-full bg-white flex justify-center items-center">
+            <Image src={photo} width={150} height={150} className="rounded-full" />
+        </div>
         <h1 className="text-xl">{name}</h1>
         <p className="text-sm">{desig}</p>
     </div>
 }
 
+const speakers = [
+    {
+        photo: '/assets/speakers/arnav ray.jpeg',
+        name: 'Arnab Ray',
+        desig: 'CEO, Array Ventures & BPlan Experts',
+    },
+    {
+        photo: '/assets/speakers/subham acharya.jpeg',
+        name: 'Subham Acharya',
+        desig: 'Director FxUAV Technologies Pvt Ltd',
+    }
+]
+
 export default function Speakers() {
     return (<>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mb-12">
             <div className="relative flex flex-col items-center justify-center w-full my-16 mt-24">
             <span className="absolute text-4xl md:text-8xl font-extrabold text-yellow-700/50 blur-lg">
                 OUR SPEAKERS
@@ -21,8 +36,8 @@ export default function Speakers() {
             <div className="mt-4 w-24 md:w-1/3 h-1 bg-gradient-to-r from-[#FFD35B] to-[#F5A201] rounded-full"></div>
             </div>
             <div className="flex w-[65vw] justify-center gap-8 my-8 flex-wrap">
-                {(new Array(10)).fill(1).map(() =>
-                    <SpeakerCard name='Mr. X' desig='XYZ' />
+                {speakers.map((sp) =>
+                    <SpeakerCard photo={sp.photo} name={sp.name} desig={sp.desig} />
                 )}
             </div>
         </div>
